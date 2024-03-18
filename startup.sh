2e6fd26b5554c7e8f542d091a42a7cdfc225eba3
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-03-17 23:06:06 -0400

    Startup Script changes

--- startup.sh
+++ startup.sh
@@ -25,13 +25,6 @@ fi
 # Change the ownership of the /opt/webapp/dist/.env file to the csye6225 user
 chown csye6225:csye6225 /opt/webapp/dist/.env
 
-# Create the log folder
-mkdir -p /var/log/webapp
-
-# Set the permissions for the log folder
-chown -R csye6225:csye6225 /var/log/webapp
-chmod -R 744 /var/log/webapp
-
 # Create the webapp_startup_completed file to indicate that the startup script has been completed once.
 # Set the read permissions for all users for the webapp_startup_completed file
 touch /opt/webapp_startup_completed
